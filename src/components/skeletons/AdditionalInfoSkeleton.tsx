85b8180c43b497e057a854e5d2839a68f86e0a49
Cam Johnson <cameron.n.johnson15@gmail.com>
2026-02-27 11:47:17 -0700

    Create skeleton

---
+++ src/components/skeletons/AdditionalInfoSkeleton.tsx
@@ -0,0 +1,20 @@
+import Card from '../cards/Card';
+import { Skeleton } from '../ui/skeleton';
+
+type Props = {};
+
+export default function AdditionalInfoSkeleton({}: Props) {
+  return (
+    <Card title='Additional Info' childrenClassName='flex flex-col gap-8'>
+      {Array.from({ length: 6 }).map((_, index) => (
+        <div className='flex justify-between' key={index}>
+          <div className='flex gap-4'>
+            <Skeleton className='w-20 h-8' />
+            <Skeleton className='size-8 rounded-full' />
+          </div>
+          <Skeleton className='size-8' />
+        </div>
+      ))}
+    </Card>
+  );
+}
---
+++ src/components/skeletons/DailySkeleton.tsx
@@ -0,0 +1,22 @@
+import Card from '../cards/Card.tsx';
+import { Skeleton } from '../ui/skeleton.tsx';
+
+type Props = {};
+
+export default function DailyForecastSkeleton({}: Props) {
+  return (
+    <Card title='Daily Forecast' childrenClassName='flex flex-col gap-4'>
+      {Array.from({ length: 8 }).map((_, index) => {
+        return (
+          <div key={index} className='flex justify-between'>
+            <Skeleton className='w-9 h-8' />
+            <Skeleton className='size-8 rounded-full' />
+            <Skeleton className='size-8' />
+            <Skeleton className='size-8' />
+            <Skeleton className='size-8' />
+          </div>
+        );
+      })}
+    </Card>
+  );
+}
