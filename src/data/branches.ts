4d52f0f7c65cde4699cc0c294c9e4e15a3fecd5b
Cam Johnson <cameron.n.johnson15@gmail.com>
2026-02-27 11:01:01 -0700

    Add branches data

---
+++ src/data/branches.ts
@@ -0,0 +1,43 @@
+export type Branch = {
+  name: string;
+  lat: number;
+  lon: number;
+};
+
+export const branches: Branch[] = [
+  { name: 'Abilene', lat: 39.696263, lon: -104.827156 },
+  { name: 'Anschutz', lat: 39.7437474, lon: -104.841302 },
+  { name: 'Arvada', lat: 39.841718, lon: -105.0814365 },
+  { name: 'Aurora', lat: 39.6382115, lon: -104.7909513 },
+  { name: 'Boardwalk', lat: 40.5258039, lon: -105.065265 },
+  { name: 'Brighton', lat: 39.9843104, lon: -104.825161 },
+  { name: 'Broomfield', lat: 39.9162961, lon: -105.052402 },
+  { name: 'Colorado Springs', lat: 38.836822, lon: -104.819204 },
+  { name: 'CSU Lory Student Center', lat: 40.572752, lon: -105.083785 },
+  { name: 'DIA', lat: 39.851026, lon: -104.672537 },
+  { name: 'Drake', lat: 40.5545686, lon: -105.0869778 },
+  { name: 'DTC', lat: 39.5924359, lon: -104.887031 },
+  { name: 'Evans', lat: 39.6789633, lon: -104.906517 },
+  { name: 'Fruita', lat: 39.150951, lon: -108.73663 },
+  { name: 'Grand Junction', lat: 39.077737, lon: -108.537975 },
+  { name: 'Greeley', lat: 40.4229093, lon: -104.7168512 },
+  { name: 'Health Science Center (HSC)', lat: 39.7296652, lon: -104.940501 },
+  { name: 'Highlands Ranch', lat: 39.54277420043945, lon: -104.9480972290039 },
+  { name: 'Lakewood', lat: 39.7242064, lon: -105.0807738 },
+  { name: 'Lemay', lat: 40.5756995, lon: -105.0573501 },
+  { name: 'Littleton', lat: 39.608937, lon: -105.109903 },
+  { name: 'Lone Tree', lat: 39.5524682, lon: -104.8736162 },
+  { name: 'Loveland', lat: 40.4194016, lon: -105.0730696 },
+  { name: 'Meadows', lat: 39.4042137, lon: -104.8868629 },
+  { name: 'Meldrum', lat: 40.5834525, lon: -105.0830378 },
+  { name: 'Parker', lat: 39.5395359, lon: -104.7774333 },
+  { name: 'Pueblo', lat: 38.308949, lon: -104.577141 },
+  { name: 'Ridge', lat: 39.378214, lon: -104.8280789 },
+  { name: 'South Broadway', lat: 39.6121924, lon: -104.9879521 },
+  { name: 'Spectrum', lat: 39.024189, lon: -104.81749 },
+  { name: 'Thornton', lat: 39.9144711, lon: -104.9809663 },
+  { name: 'Thorn Creek', lat: 39.9424572, lon: -104.9853344 },
+  { name: 'University', lat: 39.6795848, lon: -104.9596988 },
+  { name: 'Windsor', lat: 40.4793132, lon: -104.9337116 },
+  { name: 'Writer Square', lat: 39.7481907, lon: -104.9980665 },
+];
